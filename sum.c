fde6ca674e675db340fb8ccdf72161c97d022dd4
Jaychao <joshua01.chao@gmail.com>
2024-05-07 00:31:01 +0800

    big5

--- sum.c
+++ sum.c
@@ -4,11 +4,11 @@ int main()
 {   int integer1;
     int integer2;
     int sum;
-    printf("num1: ");
-    scanf("%d", integer1);
-    printf("num2: ");
-    scanf("%d", integer2);
+    printf("數字1是: ");
+    scanf("%d", &integer1);
+    printf("數字2是: ");
+    scanf("%d", &integer2);
     sum = integer1 + integer2;
-    printf("Sum is %d \n", sum);
+    printf("總和是 %d。\n", sum);
     return 0;
 }
