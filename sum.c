575c46a6cd56731d390fef31ee9d339a2b482992
Jaychao <joshua01.chao@gmail.com>
2024-05-07 00:05:16 +0800

    123

--- sum.c
+++ sum.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+
 int main()
 {   int integer1;
     int integer2;
     int sum;
-    printf("請輸入數字1: ");
+    printf("num1: ");
     scanf("%d", integer1);
-    printf("請輸入數字2: ");
+    printf("num2: ");
     scanf("%d", integer2);
     sum = integer1 + integer2;
-    printf("總和是 %d \n", sum);
+    printf("Sum is %d \n", sum);
     return 0;
 }
