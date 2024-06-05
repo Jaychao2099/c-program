49f573e7294e0c9836939fd18b2032452ef3366b
Jaychao <joshua01.chao@gmail.com>
2024-06-05 22:32:08 +0800

    while

--- sum.c
+++ sum.c
@@ -1,20 +1,31 @@
 #include <stdio.h>
 
 int main(){
-    long long int integer, sum = 0;
+    long long integer, sum = 0;
 
+    int i = 1;
+    printf("氨ゎ猭叫块 0\n");
+    while(i < 50) {
+        printf("计 %d 琌: ", i);
+        scanf("%lld", &integer);
+        if (integer == 0)
+            break;
+        sum = sum + integer;
+        i++;
+    }
+/*
     printf("计1琌: ");
-    scanf("%lld", &integer);
+    scanf("%d", &integer);
     sum = sum + integer;
 
     printf("计2琌: ");
-    scanf("%lld", &integer);
+    scanf("%d", &integer);
     sum = sum + integer;
 
     printf("计3琌: ");
-    scanf("%lld", &integer);
+    scanf("%d", &integer);
     sum = sum + integer;
-    
+*/
     printf("羆㎝琌 %lld\n瞡盾?", sum);
     return 0;
 }
