86610e74e9a3ac5446d13a195b3a3e5a48b87320
Jaychao <joshua01.chao@gmail.com>
2024-06-06 00:06:42 +0800

    delete//

--- sum.c
+++ sum.c
@@ -13,32 +13,7 @@ int main(){
         sum = sum + integer;
         i++;
     }
-/*
-    printf("计1琌: ");
-    scanf("%d", &integer);
-    sum = sum + integer;
 
-    printf("计2琌: ");
-    scanf("%d", &integer);
-    sum = sum + integer;
-
-    printf("计3琌: ");
-    scanf("%d", &integer);
-    sum = sum + integer;
-*/
     printf("羆㎝琌 %lld\n瞡盾?", sum);
     return 0;
 }
-
-/*
-int main(){
-    double num1, num2, sum;
-    printf("计1琌: ");
-    scanf("%lf", &num1);
-    printf("计2琌: ");
-    scanf("%lf", &num2);
-    sum = num1 + num2;
-    printf("羆㎝琌 %lf\n瞡盾?????", sum);
-    return 0;
-}
-*/
