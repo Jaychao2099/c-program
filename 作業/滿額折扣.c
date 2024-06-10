a3f2001abadffefd8ac9d8fbea96d7747edfe60d
Jaychao <joshua01.chao@gmail.com>
2024-06-11 00:20:26 +0800

    ok

--- 作業/滿額折扣.c
+++ 作業/滿額折扣.c
@@ -2,12 +2,12 @@
 
 int main() {
     int people, sum;
-    printf("Please enter the nummber of the people in the table: ");
+    printf("Please enter the nummber of customers in the table: ");
     scanf("%d", &people);
     sum = 300 * people;
     if (sum >= 3000) {
         sum = sum * 0.8;
     }
-    printf("The total is %d dollars.", sum);
+    printf("Total: %d dollars.", sum);
     return 0;
 }
