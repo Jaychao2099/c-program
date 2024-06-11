f2fb751ab05c8a570a17a0adf86e1481b292cb1e
Jaychao <joshua01.chao@gmail.com>
2024-06-11 23:07:10 +0800

    int

--- 勒讓德多項式.c
+++ 勒讓德多項式.c
@@ -1,29 +1,30 @@
 #include <stdio.h>
 
-float polya(float n, float x);
+double polya(int n, double x);
 
 int main() {
-    float n, x;
+    int n;
+    double x;
     
     // 取得使用者輸入
-    printf("Enter the value of n: ");
-    scanf("%f", &n);
-    printf("Enter the value of x: ");
-    scanf("%f", &x);
+    printf("Enter the value of n (∈ N): ");
+    scanf("%d", &n);
+    printf("Enter the value of x (-1 ~ 1): ");
+    scanf("%lf", &x);
     
     // 呼叫 polya 函式並印出結果
-    printf("The value of the polynomial is: %f\n", polya(n, x));
+    printf("The value of the polynomial is: %lf\n", polya(n, x));
     
     return 0;
 }
 
-float polya(float n, float x) {
+double polya(int n, double x) {
     if (n == 0) 
         return 1.0;
     if (n == 1)
         return x;
     else {
-        float result_0, result_1, change = 1., x;
+        double result_0, result_1, change = 1., x;
         for(int i = 2; i <= n; i++){
             change = result_1;
             result_1 = ((2.*i-1.)*x*result_1 - (i-1)*result_0)/i;
