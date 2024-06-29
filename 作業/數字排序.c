03543ad8101e2e58913eb7203e5d777b8a6f2207
Jaychao <joshua01.chao@gmail.com>
2024-06-29 23:49:53 +0800

    ok吧

--- 作業/數字排序.c
+++ 作業/數字排序.c
@@ -1,25 +1,25 @@
 # include <stdio.h>
 
-# define min(a, b) ((a) < (b) ? (a) : (b))
-
 int main(){
     int count;
-    scanf("Enter the number of list: %d", &count);
+    printf("Enter the number of list: ");
+    scanf("%d", &count);
+
     int list[count], sorted[count];
+
+    printf("Enter the list: ");
     for(int i = 0; i < count; i++){
         scanf("%d", &list[i]);
     }
 
     for (int j = 0; j < count; j++){
-        int smallest;
-        for(int k = 0; k < count; k++){
-            smallest = min(list[k], list[k+1]);
+        for(int k = j; k < count-1; k++){
+            list[j] = list[j] < list[k+1] ? list[j] : list[k+1];
         }
-        sorted[j] = smallest;
     }
     printf("Sorted list:");
     for(int i = 0; i < count; i++){
-        printf(" %d", sorted[i]);
+        printf(" %d", list[i]);
     }
     return 0;
 }
