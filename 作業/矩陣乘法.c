10a723dc0f94ece663f892764bb38dd99e59c6c5
Jaychao <joshua01.chao@gmail.com>
2024-07-01 18:53:48 +0800

    ok

--- 作業/矩陣乘法.c
+++ 作業/矩陣乘法.c
@@ -20,22 +20,23 @@ int main(){
         }
     }
     printf("Enter elements in B by rows:\n");
-    for (int i = 0; i < m; i++){
-        for (int j = 0; j < n; j++){
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < p; j++){
             scanf("%d", &B[i][j]);
         }
     }
-    for (int i = 0; i < m; i++){
+    for (int i = 0; i < m; i++){ //乘法部分
         for (int j = 0; j < p; j++){
+            C[i][j] = 0;  // 初始化 C[i][j]
             for (int k = 0; k < n; k++){
-                C[i][j] = C[i][j] + A[i][k]*B[k][j];
+                C[i][j] += A[i][k]*B[k][j];
             }
         }
     }
     printf("Matrix C =\n");
     for (int i = 0; i < m; i++){
         for (int j = 0; j < p; j++){
-            printf("%d ", &C[i][j]);
+            printf("%d ", C[i][j]);
         }
         printf("\n");
     }
