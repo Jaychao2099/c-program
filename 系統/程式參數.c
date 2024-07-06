624be8fb6fe4299f1d6bd8288bf3273da4f7fa50
Jaychao <joshua01.chao@gmail.com>
2024-07-06 22:38:38 +0800

    new

---
+++ 系統/程式參數.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    printf("Program name: %s\n", argv[0]);
+    
+    printf("Argument count: %d\n", argc);
+    
+    for (int i = 1; i <= argc; i++) {
+        printf("Arg %d: %s\n", i, argv[i]);
+    }
+    
+    return 0;
+}
