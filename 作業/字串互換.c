1305251a5a0398c9fdb5b2b225f105698eb6ad8d
Jaychao <joshua01.chao@gmail.com>
2024-07-11 20:23:05 +0800

    change name + ok

--- 作業/字串互換.c
+++ 作業/字串互換.c
@@ -2,17 +2,15 @@
 # include<string.h>
 # include<stdlib.h>
 
-void strcpy_B_to_A(char* x, char* y){
+void strcpyy(char* x, char* y){
     int i = 0;
-    while ((x[i] = y[i]) != '\0')
+    while ((y[i] = x[i]) != '\0')
     i++;
 }
 
 void strswitch(char* x, char* y){
     int i = 0;
-    size_t length_x = strlen(x);
-    size_t length_y = strlen(y);
-    size_t max_length = length_x > length_y ? length_x : length_y;
+    size_t max_length = strlen(x) > strlen(y) ? strlen(x) : strlen(y);
     char* temp = malloc(max_length * sizeof(char));
     for (size_t i = 0; i < max_length; i++){
         temp[i] = x[i];
@@ -23,17 +21,22 @@ void strswitch(char* x, char* y){
 }
 
 int main(){                     //ENTER AB, A COPY TO C, BC CHANGE
-    char A[20] = "";
-    char *B = "123DWED1E2";
-    char* C;
+    char A[20], B[20], C[20];
 
-    printf("A = %s\n", A);
-    printf("B = %s\n", B);
+    printf("String A: ");
+    scanf("%20s", A);
+    printf("String B: ");
+    scanf("%20s", B);
+    printf("String C: ");
+    scanf("%20s", C);
+
     printf("---------\n");
 
-    strcpy_B_to_A(A, B);
+    strcpyy(A, C);
+    strswitch(B, C);
 
     printf("A = %s\n", A);
     printf("B = %s\n", B);
+    printf("C = %s\n", C);
     return 0;
 }
