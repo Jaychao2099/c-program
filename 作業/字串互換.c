c5e13914fda6b0f438b0f6e2b3d4f9918e7ffb48
Jaychao <joshua01.chao@gmail.com>
2024-07-11 20:49:14 +0800

    test

--- 作業/字串互換.c
+++ 作業/字串互換.c
@@ -2,6 +2,8 @@
 # include<string.h>
 # include<stdlib.h>
 
+# define MAX_LENGTH 50
+
 void strcpyy(char* x, char* y){
     int i = 0;
     while ((y[i] = x[i]) != '\0')
@@ -11,7 +13,7 @@ void strcpyy(char* x, char* y){
 void strswitch(char* x, char* y){
     int i = 0;
     size_t max_length = strlen(x) > strlen(y) ? strlen(x) : strlen(y);
-    char* temp = malloc(max_length * sizeof(char));
+    char* temp = malloc((max_length + 1) * sizeof(char));
     for (size_t i = 0; i < max_length; i++){
         temp[i] = x[i];
         x[i] = y[i];
@@ -21,14 +23,25 @@ void strswitch(char* x, char* y){
 }
 
 int main(){                     //ENTER AB, A COPY TO C, BC CHANGE
-    char A[20], B[20], C[20];
+    char A[MAX_LENGTH], B[MAX_LENGTH], C[MAX_LENGTH];
 
     printf("String A: ");
-    scanf("%20s", A);
+    scanf("%s", A);
     printf("String B: ");
-    scanf("%20s", B);
+    scanf("%s", B);
     printf("String C: ");
-    scanf("%20s", C);
+    scanf("%s", C);
+
+    if (strlen(A) > MAX_LENGTH || strlen(B) > MAX_LENGTH || strlen(C) > MAX_LENGTH){
+        printf("TOO MUCH!! Enter within %d words\n", MAX_LENGTH);
+        return 1;
+    }
+
+    printf("---------\n");
+    
+    printf("A = %s\n", A);
+    printf("B = %s\n", B);
+    printf("C = %s\n", C);
 
     printf("---------\n");
 
@@ -38,5 +51,6 @@ int main(){                     //ENTER AB, A COPY TO C, BC CHANGE
     printf("A = %s\n", A);
     printf("B = %s\n", B);
     printf("C = %s\n", C);
+    
     return 0;
 }
