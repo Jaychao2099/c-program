07825a94ef82c78cfa3753266620f41384438040
Jaychao <joshua01.chao@gmail.com>
2024-07-11 16:03:51 +0800

    ok

--- 網路怪盜/路過/記憶體管理_calloc.c
+++ 網路怪盜/路過/記憶體管理_calloc.c
@@ -2,22 +2,25 @@
 #include <stdlib.h>
 #include <string.h>
 int main() {
-    int *dynArr;
-    int arrLen = 10;
+    char* dynArr_char;
+    int* dynArr_int;
+    int arrLen = 5;
 
     // 配置記憶體，並初始化
-    dynArr = calloc( arrLen, sizeof(int) );
+    dynArr_char = calloc( arrLen, sizeof(char) );
+    dynArr_int = calloc( arrLen, sizeof(int) );
 
-    if( dynArr == NULL ) {
+    if( dynArr_char == NULL || dynArr_int == NULL  ) {
         fprintf(stderr, "Error: unable to allocate required memory\n");
         return 1;
     }
     
     for (size_t i = 0; i < arrLen; ++i) {
-    printf("%d ", dynArr[i]);
+    printf("dynArr_char[%d] = \"%c\", dynArr_int[%d] = \"%d\"\n", i, dynArr_char[i], i, dynArr_int[i]);
     }
 
-    free(dynArr);
+    free(dynArr_char);
+    free(dynArr_int);
 
     return 0;
 }
