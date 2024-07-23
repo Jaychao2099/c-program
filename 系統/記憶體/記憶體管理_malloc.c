0db2db55cced6b7fb906d39b211802a2f24c646a
Jaychao <joshua01.chao@gmail.com>
2024-07-23 21:39:50 +0800

    陣列

--- 系統/記憶體/記憶體管理_malloc.c
+++ 系統/記憶體/記憶體管理_malloc.c
@@ -2,14 +2,17 @@
 #include <stdlib.h>
 
 int main() {
-    int *dynArr;
+    int **dynArr;
 
     // 指定空間大小
     int arrLen1 = 10;
     int arrLen2 = 5;
 
     // 取得記憶體空間
-    dynArr = malloc( arrLen1 * arrLen2 * sizeof(int) );
+    dynArr = (int **)malloc( arrLen1 * sizeof(int *) );
+    for (int i = 0; i < arrLen1; i++) {
+        dynArr[i] = (int *)malloc(arrLen2 * sizeof(int));
+    }
 
     if( dynArr == NULL ) {
         fprintf(stderr, "Error: unable to allocate required memory\n");
@@ -20,12 +23,19 @@ int main() {
     for (size_t i = 0; i < arrLen1; ++i) {
         for (size_t j = 0; j < arrLen2; ++j) {
             int index = i * arrLen2 + j;
+            /*
             dynArr[index] = index;
-            printf("[%d][%d] = dynArr[%d] = %d \n", i, j, index, dynArr[index]);
+            printf("[%d][%d] = dynArr[%d] = %d, add = \n", i, j, index, dynArr[index]);
+            */
+            dynArr[i][j] = index;
+            printf("dynArr[%d][%d] = %d, addr = %x\n", i, j, dynArr[i][j], &dynArr[i][j]);
         }
     }
 
     // 釋放記憶體空間
+    for (int i = 0; i < arrLen1; i++) {
+        free(dynArr[i]);  // 釋放每一行
+    }
     free(dynArr);
 
     return 0;
