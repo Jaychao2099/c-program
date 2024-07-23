6679782552867b379bff02d3635055beee4ef0be
Jaychao <joshua01.chao@gmail.com>
2024-07-23 22:13:54 +0800

    number ok

--- 作業/雜/自由分配律.c
+++ 作業/雜/自由分配律.c
@@ -15,8 +15,8 @@ int *cal_ratio(double a, double b){
     gcd_ab = gcd(a,b);
     a = a / gcd_ab;
     b = b / gcd_ab;
-    result[0] = pow(a,2) + 2*a*b;
-    result[1] = pow(b,2);
+    result[0] = a*a + 2*a*b;
+    result[1] = b*b;
     return result;
 }
 
@@ -33,29 +33,38 @@ int main(){
     
     freq = (double *)malloc(num * sizeof(double));
     ratio = (int **)malloc(num * sizeof(int*));
-    for (int i = 0; i < num; i++) ratio[i] = (int *)malloc(2 * sizeof(int));  // 分配第二層指標數組
-    result = (int *)malloc(pow(2, num) * sizeof(int));
+    for (int i = 0; i < num; i++) {
+        ratio[i] = NULL; // 初始化指針為NULL
+    }
+    result = (int *)malloc((1 << num) * sizeof(int));
     
     for (int i = 0; i < num; i++){
         printf("allel-%d dominant gene frequency (0~1): ", i+1);
         if (scanf("%lf", &freq[i]) != 1 || freq[i] < 0 || freq[i] > 1){
             printf("Plz enter 0 ~ 1");
+            free(freq);
+            for (int j = 0; j < i; j++) {
+                free(ratio[j]);
+            }
+            free(ratio);
+            free(result);
             return 1;
         }
         ratio[i] = cal_ratio(freq[i], 1 - freq[i]);
     }
 
-    for (int i = 0; i < pow(2, num); i++){
+    for (int i = 0; i < (1 << num); i++){
         result[i] = 1;
-        int temp = i/pow(2,num-i-1);
+        int temp = i;
         for (int j = 0; j < num; j++){
-            result[i] = result[i] * ratio[j][temp];
+            result[i] *= ratio[j][temp%2];
+            temp /= 2;
         }
     }
 
-    for (int i = 0; i < pow(2, num); i++){
+    for (int i = 0; i < (1 << num); i++){
         printf("%d", result[i]);
-        if (i < pow(2, num)) printf(":");
+        if (i < (1 << num)-1) printf(":");
     }
 
     free(freq);
