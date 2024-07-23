05cd2552449e6a2702ed9d7ae3346751faf5967e
Jaychao <joshua01.chao@gmail.com>
2024-07-23 23:58:31 +0800

    allele ok

--- 作業/雜/自由分配律.c
+++ 作業/雜/自由分配律.c
@@ -13,8 +13,8 @@ int *cal_ratio(double a, double b){
     a = round(a * 10000);
     b = round(b * 10000);
     gcd_ab = gcd(a,b);
-    a = a / gcd_ab;
-    b = b / gcd_ab;
+    a /= gcd_ab;
+    b /= gcd_ab;
     result[0] = a*a + 2*a*b;
     result[1] = b*b;
     return result;
@@ -24,6 +24,7 @@ int main(){
     int num;
     double *freq;
     int **ratio, *result;
+    //char *allele;
 
     printf("Number of allele (1~26): ");
     if (scanf("%d", &num) != 1 || num < 1 || num > 26){
@@ -34,9 +35,15 @@ int main(){
     freq = (double *)malloc(num * sizeof(double));
     ratio = (int **)malloc(num * sizeof(int*));
     for (int i = 0; i < num; i++) {
-        ratio[i] = NULL; // 初始化指針為NULL
+        ratio[i] = NULL; // 初始化指針為NULL, cal_ratio再分配記憶體
     }
     result = (int *)malloc((1 << num) * sizeof(int));
+    //allele = (char *)malloc((num + 1) * sizeof(char));
+
+    //for (int i = 0; i < num; i++){
+    //    allele[i] = 65 + i;
+    //}
+    
     
     for (int i = 0; i < num; i++){
         printf("allel-%d dominant gene frequency (0~1): ", i+1);
@@ -48,6 +55,7 @@ int main(){
             }
             free(ratio);
             free(result);
+            //free(allele)
             return 1;
         }
         ratio[i] = cal_ratio(freq[i], 1 - freq[i]);
@@ -56,21 +64,25 @@ int main(){
     for (int i = 0; i < (1 << num); i++){
         result[i] = 1;
         int temp = i;
+        char allele_char[num + 1];
         for (int j = 0; j < num; j++){
             result[i] *= ratio[j][temp%2];
+            allele_char[num -1 -j] =  64 + num - j + 32*(temp%2);
             temp /= 2;
         }
-    }
-
-    for (int i = 0; i < (1 << num); i++){
-        printf("%d", result[i]);
-        if (i < (1 << num)-1) printf(":");
+        for (int i = 0; i < num; i++){
+            printf("%c", allele_char[i]);
+        }
+        printf(": %d\n", result[i]);
     }
 
     free(freq);
-    for (int i = 0; i < num; i++) free(ratio[i]);
+    for (int i = 0; i < num; i++){
+        free(ratio[i]);
+    }
     free(ratio);
     free(result);
+    //free(allele)
 
     return 0;
 }
