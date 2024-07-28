e192cd4fd5afe90041d6bc24670ca2a57b8c8b76
Jaychao <joshua01.chao@gmail.com>
2024-07-28 23:07:29 +0800

    still broken

--- 作業/排序法/快速排序_quick_sort.c
+++ 作業/排序法/快速排序_quick_sort.c
@@ -1,6 +1,32 @@
 # include <stdio.h>
 # include <stdlib.h>
 
+void swap(int *v, int a, int b){
+    int temp;
+    temp = v[a];
+    v[a] = v[b];
+    v[b] = temp;
+}
+
+void sort(int *input, int num){
+    int i = 0;
+    for (int j = 0; j < num - 1; j++){
+        if(input[j] < input[num - 1]){
+            swap(input, j, i);
+            i++;
+        }
+    }
+    if (i != 0) swap(input, num - 1, i);
+
+    if (i >= num - 1)
+        return;
+    else {
+        sort(input, i);
+        sort(&input[i + 1], num - i -1);
+        return;
+    }
+}
+
 int main(){
     int count;
     printf("Enter the number of list: ");
@@ -8,8 +34,19 @@ int main(){
 
     int *list = malloc( (count+1) * sizeof(int) );
 
+    printf("Enter the list: \n");
+    for(int i = 0; i < count; i++){
+        scanf("%d", &list[i]);
+    }
 
+    sort(list, count);
+
+    printf("Sorted list:\n");
+    for(int i = 0; i < count; i++){
+        printf("%d ", list[i]);
+    }
 
     free(list);
+
     return 0;
 }
