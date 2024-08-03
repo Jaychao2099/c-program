eafa98a1dfeee328d9e2db0a0949711f9f4268cc
Jaychao <joshua01.chao@gmail.com>
2024-08-03 23:15:26 +0800

    精度調整

--- 作業/數學/進制互換_浮點.c
+++ 作業/數學/進制互換_浮點.c
@@ -6,49 +6,52 @@
 //# define MAX_frac_PRECISION 16
 //# define MAX_CHAR MAX_int_PRECISION + MAX_frac_PRECISION + 2
 
-# define MAX_CHAR 30 + 2
+# define MAX_CHAR 18
 
 //# define num2char(a) a + 48
 # define char2num(a) a - 48
 
 long double switch_base(int old, int new, char *input){
-    long double result_d = 0;
+    int integer = 0;
+    long double frac = 0, result_d = 0;
 
     int dot = 0;
     while (input[dot] != 46) dot++;     //小數點位置
+
     
+    int temp_old_int = 1;
+    long long int temp_old_frac = old;
+    for (int i = 0; i < dot; i++){              // 整數部分 變 old 進位 整數
+        integer += (int)(char2num(input[dot-i-1])) * temp_old_int;
+        temp_old_int *= old;
+    }
+    for (int i = dot + 1; i < MAX_CHAR; i++){   // 小數部分 變 old 進位 小數
+        frac += (long double)((int)char2num(input[i])) / temp_old_frac;
+        //printf("i = %d, %.21Lf += %.21Lf / %lld\n", i, frac, (long double)((int)char2num(input[i])), temp_old_frac);
+        temp_old_frac *= old;
+    }
     switch (old){
         case 10:
-            int integer = 0;
-            for (int i = 0; i < dot; i++){              // 整數部分 變 十進位 整數
-                integer += (int)char2num(input[i]) * pow(old, dot - i - 1);
-            }
+            int temp10 = 1;
             for (int i = 0; integer > 0; i++){          // 整數部分
-                result_d += (integer % new) * pow(10, i);
+                result_d += (integer % new) * temp10;
                 integer /= new;
+                temp10 *= 10;
             }
-
-            long double frac = 0;
-            for (int i = dot + 1; i < MAX_CHAR; i++){   // 小數部分 變 十進位 小數
-                frac += ((double)((int)char2num(input[i])) / pow(old, i - dot));
-                //printf("frac = %Lf\n", frac);
-            }
-
+            temp10 = 10;
             for (int i = dot + 1; i < MAX_CHAR && frac > 0; i++){  // 小數部分
                 frac *= new;
-                result_d += (((double)((int)(frac+1e-5))) / pow(10, i - dot));
+                result_d += (floor(frac+1e-5) / temp10);
                 frac = frac - ((frac > (1-1e-5)) ? 1. : 0.);
+                temp10 *= 10;
             }
-            break;
+            return result_d;
         case 2:
-            printf("Not yet support, ");
-            return NAN;    
+            return (long double)(integer) + frac;
         default:
             printf("invalid base, ");
             return NAN;
         }
-
-    return result_d;
 }
 
 int main(){
@@ -78,7 +81,18 @@ int main(){
         return 1;
     }
     
-    printf("Answer: %.18Lf", output_num);
+    
+    switch (base_new){
+        case 2:
+            printf("Answer: %.8Lf", output_num);
+            break;
+        case 10:
+            printf("Answer: %.9Lf", output_num);
+            break;
+        default:
+            printf("error: Base must be 2 or 10\n");
+            break;
+    }
 
     return 0;
 }
