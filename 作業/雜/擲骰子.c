7bb9bee9a64ff4bc95440916b176f094c7fc63aa
Jaychao <joshua01.chao@gmail.com>
2024-08-20 21:00:42 +0800

    use array

--- 作業/雜/擲骰子.c
+++ 作業/雜/擲骰子.c
@@ -10,33 +10,30 @@ int main(){
 
     srand(time(0));
 
-    int loop;
-    //int one=0, two=0, three=0, four=0, five=0, six=0;
+    int dice;
 
+    /*for (int i = 0; i < num; i++){
+        dice = rand() % 6 + 1;
+        printf("%d\n", dice);
+    }*/
+
+    int *result;
+    result = (int *)calloc(6, sizeof(int));
+    
     for (int i = 0; i < num; i++){
-        loop = rand() % 6 + 1;
-        printf("%d\n", loop);
-        /*switch (loop){
-        case 1:
-            one++;
-            break;
-        case 2:
-            two++;
-            break;
-        case 3:
-            three++;
-            break;
-        case 4:
-            four++;
-            break;
-        case 5:
-            five++;
-            break;
-        case 6:
-            six++;
-            break;
-        }*/
+        dice = rand() % 6 + 1;
+        switch (dice){
+            case 1: result[0]++; break;
+            case 2: result[1]++; break;
+            case 3: result[2]++; break;
+            case 4: result[3]++; break;
+            case 5: result[4]++; break;
+            case 6: result[5]++; break;
+        }
     }
-    //printf("one   = %d\ntwo   = %d\nthree = %d\nfour  = %d\nfive  = %d\nsix   = %d\n", one, two, three, four, five, six);
+    for (int i = 0; i < 6; i++) printf("%d: %d\n", i+1, result[i]);
+
+    free(result);
+    
     return 0;
 }
