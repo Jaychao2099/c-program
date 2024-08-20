c214c288e8ae54eda79cd073e2814524a572b93d
Jaychao <joshua01.chao@gmail.com>
2024-08-20 21:39:47 +0800

    ok

--- 作業/雜/擲骰子.c
+++ 作業/雜/擲骰子.c
@@ -1,10 +1,17 @@
 # include <stdio.h>
 # include <stdlib.h>
 # include <time.h>
+# include <math.h>
 
 int main(){
-    int num;
+    int num, side;
 
+    printf("How many side per dice? ");
+    if (scanf("%d", &side) != 1 || side > 1000){
+        printf("Error! Too large!");
+        return 1;
+    }
+    
     printf("How many dice? ");
     scanf("%d", &num);
 
@@ -13,26 +20,21 @@ int main(){
     int dice;
 
     /*for (int i = 0; i < num; i++){
-        dice = rand() % 6 + 1;
+        dice = rand() % side + 1;
         printf("%d\n", dice);
     }*/
 
     int *result;
-    result = (int *)calloc(6, sizeof(int));
-    
-    for (int i = 0; i < num; i++){
-        dice = rand() % 6 + 1;
-        switch (dice){
-            case 1: result[0]++; break;
-            case 2: result[1]++; break;
-            case 3: result[2]++; break;
-            case 4: result[3]++; break;
-            case 5: result[4]++; break;
-            case 6: result[5]++; break;
-        }
-    }
-    for (int i = 0; i < 6; i++) printf("%d: %d\n", i+1, result[i]);
+    result = (int *)calloc(side, sizeof(int));
 
+    for (int i = 0; i < num; i++){
+        dice = rand() % side;
+        for(int j = 0; j < side; j++) if (j == dice) result[j]++;
+    }
+    double avg = (double)num / (double)side;
+    for (int i = 0; i < side; i++){
+        printf("%*d: %*d (%5.1lf %)\n", (int)log10(side)+1, i+1, (int)log10(avg)+1, result[i], ((result[i]/avg)-1)*100);
+    }
     free(result);
     
     return 0;
