2dd0cac7980b591215ac28f5a231291d3eb0179e
Jaychao <joshua01.chao@gmail.com>
2024-08-28 18:00:40 +0800

    增加擴展二項式 遞迴法

--- 作業/數學/二項式係數.c
+++ 作業/數學/二項式係數.c
@@ -3,7 +3,7 @@
 
 # define int_error 42069
 
-int bin_recursive(int a, int b){
+int bin_recursive(int a, int b){        //一般二項式 遞迴
     if (a < 0 || b < 0){
         printf("Error: numbers must >= 0\n");
         return int_error;
@@ -12,11 +12,27 @@ int bin_recursive(int a, int b){
         printf("Error: 1st number must >= 2nd number\n");
         return int_error;
     }
+    
     if (b == 0 || a == b) return 1;
     else return bin_recursive(a - 1, b) + bin_recursive(a - 1, b - 1);
 }
 
-int bin_math(int m, int n){
+int bin_extend_recursive(int m, int n){     //擴展二項式 遞迴
+    if (n < 0){
+        printf("Error: 2nd numbers must >= 0\n");
+        return int_error;
+    }
+    if (m < n){
+        printf("Error: 1st number must >= 2nd number\n");
+        return int_error;
+    }
+    
+    if (n == 0 || m == n) return 1;
+    if (n == 1 || n == m - 1) return m;
+    else return bin_extend_recursive(m - 1, n) + bin_extend_recursive(m - 1, n - 1);
+}
+
+int bin_extend_iterative(int m, int n){     //擴展二項式 迭代
     if (n < 0){
         printf("Error: 2nd number must >= 0\n");
         return int_error;
@@ -46,7 +62,7 @@ int main(){
     }
     
     //result = bin_recursive(num1, num2);
-    result = bin_math(num1, num2);
+    result = bin_extend_iterative(num1, num2);
 
     if (result == int_error) return 1;
     else printf("C(%d, %d) = %d", num1, num2, result);
