b8b73812b84965d5972b548794918317560cdf0e
Jaychao <joshua01.chao@gmail.com>
2024-08-28 14:24:51 +0800

    int nan code

--- 作業/數學/二項式係數.c
+++ 作業/數學/二項式係數.c
@@ -1,9 +1,16 @@
 # include <stdio.h>
+# include <math.h>
+
+# define int_error 42069
 
 int bin_recursive(int a, int b){
     if (a < 0 || b < 0){
-        printf("Error: numbers must >= 1");
-        return 1;
+        printf("Error: numbers must >= 0\n");
+        return int_error;
+    }
+    if (a < b){
+        printf("Error: 1st number must >= 2nd number\n");
+        return int_error;
     }
     if (b == 0 || a == b) return 1;
     else return bin_recursive(a - 1, b) + bin_recursive(a - 1, b - 1);
@@ -11,8 +18,12 @@ int bin_recursive(int a, int b){
 
 int bin_math(int m, int n){
     if (n < 0){
-        printf("Error: 2nd number must >= 1");
-        return 1;
+        printf("Error: 2nd number must >= 0\n");
+        return int_error;
+    }
+    if (m < n && m > 0){
+        printf("Error: 1st number must >= 2nd number\n");
+        return int_error;
     }
     if (n == 0) return n;
     else {
@@ -28,7 +39,7 @@ int bin_math(int m, int n){
 int main(){
     int num1, num2;
     int result;
-    printf("Enter two positive integer: ");
+    printf("Enter two integer: ");
     if (scanf("%d", &num1) != 1 || scanf("%d", &num2) != 1){
         printf("Error\n");
         return 1;
@@ -37,6 +48,8 @@ int main(){
     //result = bin_recursive(num1, num2);
     result = bin_math(num1, num2);
 
-    printf("C(%d, %d) = %d", num1, num2, result);
+    if (result == int_error) return 1;
+    else printf("C(%d, %d) = %d", num1, num2, result);
+    
     return 0;
 }
