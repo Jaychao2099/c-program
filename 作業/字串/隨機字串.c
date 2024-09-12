ad76d549a063d149623a6014cf7c67a55fa76e94
Jaychao <joshua01.chao@gmail.com>
2024-09-13 01:34:24 +0800

    change UI

--- 作業/字串/隨機字串.c
+++ 作業/字串/隨機字串.c
@@ -5,9 +5,35 @@
 
 #define MAX_LENGTH 1000000
 #define MAX_FILENAME 256
-#define STR_LETTER "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-#define STR_DNA "ATGC"
-#define STR_RNA "AUGC"
+
+typedef struct letter_set{
+    char *name;
+    char *charlist;
+}letter_set;
+
+char *choose_set(void){
+    int total_sets = 3;     //增加 set 時修改
+    letter_set *sets = malloc(total_sets * sizeof(letter_set));
+    if (sets == NULL){
+        printf("Error allocating memory for sets\n");
+        exit(1);
+    }
+    sets[0].name = "LETTER"; sets[0].charlist = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    sets[1].name = "DNA"; sets[1].charlist = "ATGC";
+    sets[2].name = "RNA"; sets[2].charlist = "AUGC";
+
+    printf("Choose a letter set: ");
+    for (int i = 0; i < total_sets; i++) printf("%d. %s ", i + 1, sets[i].name);
+    int i = 1;
+    printf("("); while (i < total_sets) printf("%d/", i++); printf("%d): ", i);
+    int set_num;
+    do {
+        scanf("%d", &set_num);
+        if (set_num < 1 || set_num > total_sets) printf("ERROR: invalid number. Choose again: ");
+        else break;
+    } while (1);
+    return sets[set_num].charlist;
+}
 
 void generate_random_string(char *str, const char *charset, int length) {
     int charset_length = strlen(charset);
@@ -28,7 +54,6 @@ int file_exists(const char *filename) {
 }
 
 int main() {
-    char charset[] = STR_RNA;
     char random_string[MAX_LENGTH + 1];
     char filename[MAX_FILENAME];
     int length;
@@ -36,6 +61,8 @@ int main() {
 
     // 初始化隨機數生成器
     srand(time(NULL));
+
+    char *charset = choose_set();
 
     // 獲取用戶輸入的檔案名稱並檢查是否存在
     do {
@@ -45,18 +72,13 @@ int main() {
         if (file_exists(filename)) {
             printf("File '%s' already exists. Overwrite? (y/n): ", filename);
             scanf(" %c", &overwrite_choice);
-            if (overwrite_choice == 'y' || overwrite_choice == 'Y') {
-                break;
-            }
-        } else {
-            break;
-        }
+            if (overwrite_choice == 'y' || overwrite_choice == 'Y') break;
+        } else break;
     } while (1);
 
     // 獲取用戶輸入的字符串長度
     printf("String length (max %d): ", MAX_LENGTH);
     scanf("%d", &length);
-
     if (length <= 0 || length > MAX_LENGTH) {
         printf("ERROR: invalid length, must be 1 ~ %d\n", MAX_LENGTH);
         return 1;
