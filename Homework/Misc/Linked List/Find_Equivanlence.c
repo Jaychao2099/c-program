029fae0f839670b0feaaaece304ad81b3830638d
Jaychao <joshua01.chao@gmail.com>
2024-09-19 23:56:59 +0800

    ok

--- Homework/Misc/Linked List/Find_Equivanlence.c
+++ Homework/Misc/Linked List/Find_Equivanlence.c
@@ -15,8 +15,8 @@ typedef struct{
 }List;
 
 typedef struct{
-    int bool;
-    List *list;
+    int visited;
+    List list;
 }Sequence;
 
 
@@ -60,32 +60,30 @@ void remove_list(List *list){
     list->last = NULL; 
 }
 
-void find_equivanlence(Equivanlence* eq, Sequence** seq){
-    for (int i = 0; i < ARRAY_SIZE(eq); i++){
-        seq[eq[i].a]->list->head = seq[eq[i].a]->list->last = NULL;
-        seq[eq[i].b]->list->head = seq[eq[i].b]->list->last = NULL;
-        append_node(seq[eq[i].a]->list, create_node(eq[i].b));
-        append_node(seq[eq[i].b]->list, create_node(eq[i].a));
+void find_equivalence(Equivanlence *eq, int eq_size, Sequence *seq, int seq_size){
+    for (int i = 0; i < eq_size; i++){
+        append_node(&seq[eq[i].a].list, create_node(eq[i].b));
+        append_node(&seq[eq[i].b].list, create_node(eq[i].a));
     }
-    node **stack = malloc(ARRAY_SIZE(seq) * sizeof(node*));
+
+    node **stack = malloc(seq_size * sizeof(node*));
     int top = -1;
-    for (int i = 0; i < ARRAY_SIZE(seq); i++){
-        if (seq[i]->bool == 0){
-            printf("{%d, ", i);
-            seq[i]->bool = 1;
-            node *current = seq[i]->list->head;
+
+    for (int i = 0; i < seq_size; i++){
+        if (!seq[i].visited){
+            printf("{%d", i);
+            seq[i].visited = 1;
+            node *current = seq[i].list.head;
             while (current != NULL || top > -1){
-                if (seq[current->value]->bool == 0){
-                    printf("%d, ", current->value);
-                    stack[++top] = current;
-                    current = seq[current->value]->list->head;
-                    seq[current->value]->bool = 1;
-                }
-                else {
-                    if (current->next == NULL){
-                        current = stack[top--]->next;
-                    }
-                    else current = current->next;
+                if (current != NULL && !seq[current->value].visited){
+                    printf(", %d", current->value);
+                    seq[current->value].visited = 1;
+                    stack[++top] = current->next;
+                    current = seq[current->value].list.head;
+                } else {
+                    if (current == NULL && top > -1){
+                        current = stack[top--];
+                    } else current = current ? current->next : NULL;
                 }
             }
             printf("}\n");
@@ -96,22 +94,26 @@ void find_equivanlence(Equivanlence* eq, Sequence** seq){
 
 int main(){
     Equivanlence eq[] = {{0, 4}, 
-                          {3, 1},
-                          {6, 10},
-                          {8, 9},
-                          {7, 4},
-                          {6, 8},
-                          {3, 5},
-                          {2, 11},
-                          {11, 0}};
+                         {3, 1},
+                         {6, 10},
+                         {8, 9},
+                         {7, 4},
+                         {6, 8},
+                         {3, 5},
+                         {2, 11},
+                         {11, 0}};
+    int eq_size = sizeof(eq) / sizeof(eq[0]);
     int max = 0;
     for (int i = 0; i < ARRAY_SIZE(eq); i++){
         max = MAX(max, MAX(eq[i].a, eq[i].b));
     }
-    Sequence **sequence = calloc(max + 1, sizeof(Sequence));
-    find_equivanlence(eq, sequence);
-    for (int i = 0; i < max + 1; i++){
-        remove_list(sequence[i]->list);
+    int seq_size = max + 1;
+    Sequence *sequence = calloc(seq_size, sizeof(Sequence));
+    
+    find_equivalence(eq, eq_size, sequence, seq_size);
+
+    for (int i = 0; i < seq_size; i++){
+        remove_list(&sequence[i].list);
     }
     free(sequence);
     return 0;
