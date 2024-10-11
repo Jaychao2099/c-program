095f1dc4ea0f714910705eced91667f9c05c8e95
Jaychao <joshua01.chao@gmail.com>
2024-10-11 17:01:21 +0800

    add adjust model

--- Homework/Misc/Heap_example.c
+++ Homework/Misc/Heap_example.c
@@ -51,55 +51,43 @@ void insert_maxheap(heap *h, int var){
     h->array[i] = var;
 }
 
-int delete_maxheap(heap *h){
-    if (heap_empty(h)){
-        printf("ERROR: cannot delete element\n");
-        return 0;
+void adjust(int *data, int subroot_index, int size){
+    int temp = data[subroot_index];             // 先暫存，之後放到正確位置
+    int child = 2 * subroot_index + 1;
+    while(child < size){         // 還有child
+        if (child < size - 1)
+            if (data[child] < data[child + 1]) child++;
+        if (temp > data[child]) break;          // 比完了
+        data[(child - 1) / 2] = data[child];    // 沒比贏，下面換上來
+        child = 2 * child + 1;      // to left child
     }
-    int x = h->array[0];
-    int temp = h->array[--h->size];             // 最後一格提取出來當root 往下比大小
-    int parent = 0;
-    int child = 1;       // left child
-    while (child < h->size){
-        if (child < h->size - 1)
-            if (h->array[child] < h->array[child + 1]) child++; // j change to right child
-        if (temp > h->array[child]) break;          // 比完了
-        h->array[parent] = h->array[child];
-        parent = child;              // i to new parent
-        child = 2 * child + 1;      // j to left child
-    }
-    h->array[parent] = temp;
-    return x;
+    data[(child - 1) / 2] = temp;   // while loop最後，child多下一層，現在返回
 }
 
 void create_maxheap_bottom_up(heap *h, int *data){
     h->array = malloc(MAXSIZE * sizeof(int));
     for (int i = h->size / 2 - 1; i > -1; i--){     // 先到最後一個parent
-        int temp = data[i];             // 先暫存，之後放到正確位置
-        int child = 2 * i + 1;
-        while(child < h->size){         // 還有child
-            if (child < h->size - 1)
-                if (data[child] < data[child + 1]) child++;
-            if (temp > data[child]) break;          // 比完了
-            data[(child - 1) / 2] = data[child];    // 沒比贏，下面換上來
-            child = 2 * child + 1;      // to left child
-        }
-        data[(child - 1) / 2] = temp;   // while loop最後，child多下一層，現在返回
+        adjust(data, i, h->size);
     }
     memcpy(h->array, data, h->size * sizeof(int));
+}
+
+int delete_maxheap(heap *h){
+    if (heap_empty(h)){
+        printf("ERROR: cannot delete element\n");
+        return 0;
+    }
+    int x = h->array[0];   // 返回值
+    adjust(h->array, 0, --h->size);
+    return x;
 }
 
 int main(){
     int data[] = {1, 3, 2, 4, 5, 6};
     heap *h = malloc(sizeof(heap));
     h->size = sizeof(data) / sizeof(data[0]);
-    
+
     create_maxheap_bottom_up(h, data);
-    // insert_maxheap(h, 20);
-    // insert_maxheap(h, 15);
-    // insert_maxheap(h, 2);
-    // insert_maxheap(h, 14);
-    // insert_maxheap(h, 10);
     print_heap(h);
 
     printf("\n");
