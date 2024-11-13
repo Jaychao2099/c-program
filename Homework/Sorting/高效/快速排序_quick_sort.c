4eeeb4db8b5daa347e21657a06bb947a022eaf1d
Jaychao <joshua01.chao@gmail.com>
2024-11-13 18:34:36 +0800

    new method

--- Homework/Sorting/高效/快速排序_quick_sort.c
+++ Homework/Sorting/高效/快速排序_quick_sort.c
@@ -10,29 +10,43 @@ void swap(int *v, int a, int b){
     }
 }
 
-void q_sort(int *input, int start, int end){
-    while (start < end){
-        int i = start;
-        for (int j = start; j < end; j++){
-            if(input[j] < input[end]){
-                swap(input, j, i);
-                i++;
-            }
-        }
-        swap(input, end, i);
+// void q_sort(int *list, int start, int end){
+//     while (start < end){
+//         int i = start;
+//         for (int j = start; j < end; j++){
+//             if(list[j] < list[end]){      // end 當 pivot
+//                 swap(list, j, i);
+//                 i++;
+//             }
+//         }
+//         swap(list, end, i);
 
-        if (i - start < end - i) {
-            q_sort(input, start, i - 1);  //對較小的分區進行 遞迴調用
-            start = i + 1;              //對較大的分區使用 while 迭代
-        } else {
-            q_sort(input, i + 1, end);    //同上
-            end = i - 1;
-        }
+//         if (i - start < end - i) {
+//             q_sort(list, start, i - 1);  //對較小的分區進行 遞迴調用
+//             start = i + 1;                //對較大的分區使用 while 迭代
+//         } else {
+//             q_sort(list, i + 1, end);    //同上
+//             end = i - 1;
+//         }
+//     }
+// }
 
-        //q_sort(input, 0, i-1);
-        //q_sort(input, i+1, end);
-        //return;
+void q_sort(int *list, int start, int end){ 
+    if (start >= end) return;
+
+    int pivot = list[start];    //(start + end) / 2; // 使用 list[mid] 作為 pivot 值
+    int i = start, j = end;
+    while (i <= j){
+        while (list[i] < pivot) i++;    // 找到左側大於 pivot 的元素
+        while (list[j] > pivot) j--;    // 找到右側小於 pivot 的元素
+        if (i <= j){           // 如果 i < j，則交換位置
+            swap(list, i, j);
+            i++;
+            j--;
+        }
     }
+    q_sort(list, start, j); // 處理左側
+    q_sort(list, i, end);   // 處理右側
 }
 
 int main(){
@@ -40,9 +54,9 @@ int main(){
     printf("Enter the number of list: ");
     scanf("%d", &count);
 
-    int *list = malloc( (count+1) * sizeof(int) );
+    int *list = malloc(count * sizeof(int));
 
-    printf("Enter the list: \n");
+    printf("Enter the list: ");
     for(int i = 0; i < count; i++){
         scanf("%d", &list[i]);
     }
