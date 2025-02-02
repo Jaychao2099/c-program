34a7c2d26ac09ec7d9ec24babb2bb1c1eaa3625d
Jaychao <joshua01.chao@gmail.com>
2025-02-03 00:05:31 +0800

    新增快速排序演算法實作，包含計數功能及輸入處理

---
+++ Homework/Sorting/高效/快速排序_quick_sort_count.c
@@ -0,0 +1,86 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+void swap(int *v, int a, int b){
+    if (a != b){
+        int temp;
+        temp = v[a];
+        v[a] = v[b];
+        v[b] = temp;
+    }
+}
+
+// void q_sort(int *list, int start, int end){
+//     while (start < end){
+//         int i = start;
+//         for (int j = start; j < end; j++){
+//             if(list[j] < list[end]){      // end 當 pivot
+//                 swap(list, j, i);
+//                 i++;
+//             }
+//         }
+//         swap(list, end, i);
+//         if (i - start < end - i) {
+//             q_sort(list, start, i - 1);  //對較小的分區進行 遞迴調用
+//             start = i + 1;                //對較大的分區使用 while 迭代
+//         } else {
+//             q_sort(list, i + 1, end);    //同上
+//             end = i - 1;
+//         }
+//     }
+// }
+
+int count = 0;
+void q_sort(int *list, int start, int end){ 
+    if (start >= end) return;
+
+    int pivot = list[start];    //(start + end) / 2; // 使用 list[mid] 作為 pivot 值
+    
+    printf("start: %d, end: %d, pivot: %d, ", start, end, pivot);
+
+    int i = start, j = end;
+    while (i <= j){
+        while (list[i] < pivot) {count++; i++;}    // 找到左側大於 pivot 的元素
+        while (list[j] > pivot) {count++; j--;}    // 找到右側小於 pivot 的元素
+        if (i <= j){           // 如果 i < j，則交換位置
+            swap(list, i, j);
+            i++;
+            j--;
+        }
+    }
+    
+    for (int k = 0; k <= 5; k++){
+        printf("%d ", list[k]);
+    }
+    printf(", i = %d, j = %d", i, j);
+    printf(", count: %d\n", count);
+    // printf("\n");
+
+    q_sort(list, start, j); // 處理左側
+    q_sort(list, i, end);   // 處理右側
+}
+
+int main(){
+    int count;
+    printf("Enter the number of list: ");
+    scanf("%d", &count);
+
+    int *list = malloc(count * sizeof(int));
+
+    printf("Enter the list: ");
+    for(int i = 0; i < count; i++){
+        scanf("%d", &list[i]);
+    }
+
+    q_sort(list, 0, count-1);
+
+    // printf("count: %d\n", count);
+    printf("Sorted list:\n");
+    for(int i = 0; i < count; i++){
+        printf("%d ", list[i]);
+    }
+
+    free(list);
+
+    return 0;
+}
