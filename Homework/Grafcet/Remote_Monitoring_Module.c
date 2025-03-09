6cbe8404af911936c8309dd69d08b332a83ba22c
Jaychao <joshua01.chao@gmail.com>
2025-03-09 15:50:28 +0800

    重構狀態變數和函式名稱，增加初始化函式以提高可讀性

--- Homework/Grafcet/Remote_Monitoring_Module.c
+++ Homework/Grafcet/Remote_Monitoring_Module.c
@@ -4,28 +4,48 @@
 #include <unistd.h>
 
 // 定義狀態變數
-bool st_430 = true;     // 初始狀態（430）
-bool st_431 = false;    // 狀態431
-bool st_432 = false;    // 狀態432
-bool st_433 = false;    // 狀態433
-bool st_434 = false;    // 狀態434
-bool st_435 = false;    // 狀態435
-bool st_436 = false;    // 狀態436
-bool st_437 = false;    // 狀態437
-bool st_438 = false;    // 狀態438
+bool st_430;     // 初始狀態（430）
+bool st_431;    // 狀態431
+bool st_432;    // 狀態432
+bool st_433;    // 狀態433
+bool st_434;    // 狀態434
+bool st_435;    // 狀態435
+bool st_436;    // 狀態436
+bool st_437;    // 狀態437
+bool st_438;    // 狀態438
 
 // 定義條件變數
-bool 系統啟動 = false;
-bool 檢測到緊急情況 = false;
-bool 通訊請求 = false;
-bool 人員到場 = false;
-bool 通訊驗證成功 = false;
-bool 通訊驗證失敗 = false;
-bool 結束_通訊或緊急處理 = false;
+bool 系統啟動;
+bool 檢測到緊急情況;
+bool 通訊請求;
+bool 人員到場;
+bool 通訊驗證成功;
+bool 通訊驗證失敗;
+bool 結束_通訊或緊急處理;
+
+void init(){
+    st_430 = true;     // 初始狀態（430）
+    st_431 = false;    // 狀態431
+    st_432 = false;    // 狀態432
+    st_433 = false;    // 狀態433
+    st_434 = false;    // 狀態434
+    st_435 = false;    // 狀態435
+    st_436 = false;    // 狀態436
+    st_437 = false;    // 狀態437
+    st_438 = false;    // 狀態438
+
+    系統啟動 = false;
+    檢測到緊急情況 = false;
+    通訊請求 = false;
+    人員到場 = false;
+    通訊驗證成功 = false;
+    通訊驗證失敗 = false;
+    結束_通訊或緊急處理 = false;
+}
 
 // 函式原型宣告
-void grafcet0();
-void action0();
+void grafcet_43();
+void action_43();
 static inline void s430();
 static inline void s431();
 static inline void s432();
@@ -36,26 +56,27 @@ static inline void s436();
 static inline void s437();
 static inline void s438();
 // 初始化影音/網路
-void grafcet1();
-void action1();
+void grafcet_431();
+void action_431();
 // 分析檢測緊急情況
-void grafcet2();
-void action2();
+void grafcet_432();
+void action_432();
 // 緊急處置系統
-void grafcet3();
-void action3();
+void grafcet_434();
+void action_434();
 // 網路通訊系統
-void grafcet4();
-void action4();
+void grafcet_437();
+void action_437();
 
 void main(){
+    init();
     while(1){
-        grafcet0();
+        grafcet_43();
     }
 }
 
 // 依據各狀態變數的值，改變狀態變數(前一狀態變false, 下一狀態變true)
-void grafcet0(){
+void grafcet_43(){
     // 從系統啟動到初始化完成
     if(st_430 && 系統啟動){
         系統啟動 = false;
@@ -121,11 +142,11 @@ void grafcet0(){
     }
     
     // 執行動作
-    action0();
+    action_43();
 }
 
 // 依據各狀態變數，執行相應動作
-void action0(){
+void action_43(){
     if(st_430){s430();}
     if(st_431){s431();}
     if(st_432){s432();}
@@ -146,13 +167,13 @@ static inline void s430(){
 
 static inline void s431(){
     printf("state 431: Initialize monitoring and networking\n");
-    grafcet1(); // 初始化影音/網路 sub-grafcet
+    grafcet_431(); // 初始化影音/網路 sub-grafcet
     sleep(1);   // 模擬 初始化
 }
 
 static inline void s432(){
     printf("state 432: Analyze and detect emergency situations\n");
-    grafcet2(); // 分析檢測緊急情況 sub-grafcet
+    grafcet_432(); // 分析檢測緊急情況 sub-grafcet
     sleep(1);   // 模擬 通訊請求 或 檢測到緊急情況
     檢測到緊急情況 = (rand() % 2) == 1;
     通訊請求 = 檢測到緊急情況 ? false : true;
@@ -167,7 +188,7 @@ static inline void s433(){
 
 static inline void s434(){
     printf("state 434: Activation of the Emergency Response System\n");
-    grafcet3(); // 緊急處置系統 sub-grafcet
+    grafcet_434(); // 緊急處置系統 sub-grafcet
     sleep(1);   // 模擬 開啟緊急處置
     人員到場 = true;
 }
@@ -186,7 +207,7 @@ static inline void s436(){
 
 static inline void s437(){
     printf("state 437: Network communication is in progress\n");
-    grafcet4(); // 網路通訊系統 sub-grafcet
+    grafcet_437(); // 網路通訊系統 sub-grafcet
     sleep(1); // 模擬 通訊中
     結束_通訊或緊急處理 = true;
 }
@@ -198,14 +219,14 @@ static inline void s438(){
 }
 
 // 初始化影音/網路
-void grafcet1(){action1();};
-void action1(){};
+void grafcet_431(){action_431();};
+void action_431(){};
 // 分析檢測緊急情況
-void grafcet2(){action2();};
-void action2(){};
+void grafcet_432(){action_432();};
+void action_432(){};
 // 緊急處置系統
-void grafcet3(){action3();};
-void action3(){};
+void grafcet_434(){action_434();};
+void action_434(){};
 // 網路通訊系統
-void grafcet4(){action4();};
-void action4(){};
+void grafcet_437(){action_437();};
+void action_437(){};
